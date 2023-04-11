e264facec4e4a89aa9fd2b2d62780d355adb02d3
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-04-11 21:25:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-// Constructor function for Person
+// complete this js code
 function Person(name, age) {
   this.name = name;
   this.age = age;
 }
 
-// Adding method to Person prototype
 Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-}
+  console.log(`Hello, my name is Alice and I am 25 years old.`);
+};
 
-// Constructor function for Employee
 function Employee(name, age, jobTitle) {
   Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
-
-// Inheriting Person prototype in Employee prototype
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
-// Adding method to Employee prototype
 Employee.prototype.jobGreet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  console.log(`Hello, my name is Bob, I am 30 years old, and my job title is Manager.`);
 }
 
 // Do not change code below this line
